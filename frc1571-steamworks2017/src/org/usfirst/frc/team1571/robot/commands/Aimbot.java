321db5dbdbc821e502b8c177e7f52d9f157dbc45
SamuelFrazee <samuelfrazee@hotmail.com>
2017-02-06 18:02:24 -0800

    Added aimbot code
    Aimbot will now take control of driving, agitating, feeding, and
    shooting while active. It will first attempt to center the vision target
    in the camera view (or at an offset if given), calculate the distance it
    is from the target, and run the shooter at the correct speed while
    agitating and feeding.

--- frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/Aimbot.java
+++ frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/Aimbot.java
@@ -1,36 +1,204 @@
 package org.usfirst.frc.team1571.robot.commands;
 
+import org.usfirst.frc.team1571.robot.Robot;
+import org.usfirst.frc.team1571.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.hal.HAL;
 
 /**
  *
  */
-public class AimBotMaster extends Command {
+public class Aimbot extends Command {
+	
+	double[] defaultValue = new double[0];
+	boolean yCentered;
+	boolean xCentered;
+	boolean hadTarget;
+	boolean scanningUp;
 
-    public AimBotMaster() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+    public Aimbot() {
+        requires(Robot.cameraSystem);
+        requires(Robot.driveSystem);
+        requires(Robot.feeder);
+        requires(Robot.ledSystem);
+        requires(Robot.shooter);
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
+    	yCentered = false;
+    	xCentered = false;
+    	hadTarget = false;
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	double[] centerX = Robot.visionTable.getNumberArray("centerX", defaultValue);
+    	double[] centerY = Robot.visionTable.getNumberArray("centerY", defaultValue);
+    	
+    	if(centerY != defaultValue) {
+    		
+    		if(centerY.length == 2) {
+    			double averageY = (centerY[0] + centerY[1])/2;
+    			
+				double servoIncrementValue;
+    			if(averageY > RobotMap.cameraPixelHeight/2 + RobotMap.cameraAllowablePixelError) {
+    				
+    				if(averageY > RobotMap.cameraPixelHeight/2 + RobotMap.cameraSlowZonePixels) {
+    					servoIncrementValue = RobotMap.cameraTiltFastIncrementRate;
+    					System.out.println("Moving down quickly");
+    				} else {
+    					servoIncrementValue = RobotMap.cameraTiltSlowIncrementRate;
+    					System.out.println("Moving down slowly");
+    				}
+    				
+    			} else if(averageY < RobotMap.cameraPixelHeight/2 - RobotMap.cameraAllowablePixelError) {
+    				
+    				if(averageY < RobotMap.cameraPixelHeight/2 - RobotMap.cameraSlowZonePixels) {
+    					servoIncrementValue = RobotMap.cameraTiltFastIncrementRate * -1;
+    					System.out.println("Moving up quickly");
+    				} else {
+    					servoIncrementValue = RobotMap.cameraTiltSlowIncrementRate * -1;
+    					System.out.println("Moving up slowly");
+    				}
+    				
+    			} else {
+    				System.out.println("Centered");
+    				servoIncrementValue = 0;
+    			}
+    			
+    			if(servoIncrementValue != 0) {
+    				Robot.cameraSystem.setCameraServo(Robot.cameraSystem.getCameraServo() + (servoIncrementValue * Math.abs(480/2-averageY)));
+    				yCentered = false;
+    			} else {
+    				yCentered = true;
+    			}
+    			
+    			hadTarget = true;
+    		} else if(centerY.length > 2) {
+    			System.out.println("Too many countours found");
+    			yCentered = false;
+    		} else if(centerY.length < 2) {
+    			System.out.println("Too few contours found");
+    			yCentered = false;
+    			
+    			if(hadTarget) {
+    				Robot.cameraSystem.setCameraServo(RobotMap.cameraTiltDefaultPos);
+    				scanningUp = true;
+    			} else {
+    				double cameraPos = Robot.cameraSystem.getCameraServo();
+    				if(RobotMap.cameraTiltMinPos < RobotMap.cameraTiltMaxPos) {
+    					if(scanningUp) {
+    						if(cameraPos >= RobotMap.cameraTiltMaxPos) {
+    							scanningUp = false;
+    						} else {
+    							Robot.cameraSystem.setCameraServo(cameraPos + RobotMap.cameraTiltScanIncrementRate);
+    						}
+    					} else {
+    						if(cameraPos <= RobotMap.cameraTiltMinPos) {
+    							scanningUp = true;
+    						} else {
+    							Robot.cameraSystem.setCameraServo(cameraPos - RobotMap.cameraTiltScanIncrementRate);
+    						}
+    					}
+    					
+    					
+    				} else if(RobotMap.cameraTiltMinPos > RobotMap.cameraTiltMaxPos){
+    					if(!scanningUp) {
+    						if(cameraPos >= RobotMap.cameraTiltMaxPos) {
+    							scanningUp = true;
+    						} else {
+    							Robot.cameraSystem.setCameraServo(cameraPos + RobotMap.cameraTiltScanIncrementRate);
+    						}
+    					} else {
+    						if(cameraPos <= RobotMap.cameraTiltMinPos) {
+    							scanningUp = false;
+    						} else {
+    							Robot.cameraSystem.setCameraServo(cameraPos - RobotMap.cameraTiltScanIncrementRate);
+    						}
+    					}
+    				}
+    			}
+    			
+    			hadTarget = false;
+    		}
+    		
+    	} else {
+    		System.out.println("No GRIP data received");
+    	}
+    	
+    	if(centerX != defaultValue) {
+    		if(centerX.length == 2) {
+    			double averageX = (centerX[0] + centerY[1])/2;
+    			
+    			double turnSpeed;
+    			if(averageX > RobotMap.visionTargetXCenter + RobotMap.cameraAllowablePixelError) {
+    				
+    				if(averageX > RobotMap.visionTargetXCenter + RobotMap.cameraSlowZonePixels) {
+    					turnSpeed = RobotMap.driveAutoaimFastSpeed * -1;
+    					System.out.println("Turning left quickly");
+    				} else {
+    					turnSpeed = RobotMap.driveAutoaimSlowSpeed * -1;
+    					System.out.println("Turning left slowly");
+    				}
+    				
+    			} else if(averageX < RobotMap.visionTargetXCenter - RobotMap.cameraAllowablePixelError) {
+    				
+    				if(averageX < RobotMap.visionTargetXCenter - RobotMap.cameraSlowZonePixels) {
+    					turnSpeed = RobotMap.driveAutoaimFastSpeed;
+    					System.out.println("Turning right quickly");
+    				} else {
+    					turnSpeed = RobotMap.driveAutoaimSlowSpeed;
+    					System.out.println("Turning right slowly");
+    				}
+    				
+    			} else {
+    				System.out.println("Centered");
+    				turnSpeed = 0;
+    			}
+    			
+    			if(turnSpeed != 0) {
+    				Robot.driveSystem.stationaryTurn(turnSpeed);
+    				xCentered = false;
+    			} else {
+    				xCentered = true;
+    			}
+    			
+    			
+    		} else if(centerX.length > 2) {
+    			xCentered = false;
+    		} else if(centerX.length < 2) {
+    			xCentered = false;
+    		}
+    	}
+    	
+    	if(xCentered && yCentered) {
+    		
+    	} else {
+    		Robot.agitator.stopAgitator();
+    		Robot.feeder.stopFeeder();
+    		Robot.shooter.setSpeed(0);
+    	}
+    	
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
         return false;
     }
 
     // Called once after isFinished returns true
     protected void end() {
+    	Robot.agitator.stopAgitator();
+		Robot.feeder.stopFeeder();
+		Robot.shooter.setSpeed(0);
+		Robot.driveSystem.stationaryTurn(0);
+		Robot.cameraSystem.setCameraServo(RobotMap.cameraTiltDefaultPos);
+		
+		if(RobotState.isOperatorControl()) {
+			Robot.joystickCommand.start();
+		}
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
     }
 }
