311c9ecaf60e157c7111007be1a7046e2657e534
SamuelFrazee <samuelfrazee@hotmail.com>
2017-02-06 18:05:28 -0800

    Added camera distance calculation, movement, and light ring support
    CameraSystem now has functions to calculate distance to the vision
    target based on the camera servo angle, has support for a relay (SPIKE)
    that controls a light ring, and support for getting and setting the
    camera servo tilt angle.

--- frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/subsystems/CameraSystem.java
+++ frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/subsystems/CameraSystem.java
@@ -1,5 +1,10 @@
 package org.usfirst.frc.team1571.robot.subsystems;
 
+import org.usfirst.frc.team1571.robot.Robot;
+import org.usfirst.frc.team1571.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.Relay;
+import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
@@ -7,15 +12,33 @@ import edu.wpi.first.wpilibj.command.Subsystem;
  */
 public class CameraSystem extends Subsystem {
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+	private final Relay ringLight = RobotMap.ringLight;
+	private final Servo tiltServo = RobotMap.cameraTiltServo;
 
     public void initDefaultCommand() {
-		setDefaultCommand(null);
     }
     
-    public double getVisionDistance() {
-    	return 0;
+    public double getTargetDistance() {
+    	double targetAngle = (tiltServo.get()-RobotMap.cameraTiltMinPos)/(RobotMap.cameraTiltMaxPos-RobotMap.cameraTiltMinPos)*RobotMap.cameraTiltMaxAngle+RobotMap.cameraTiltMinAngle;
+		double targetDistance = RobotMap.cameraAngleFunctionA * Math.pow(targetAngle, 2) + RobotMap.cameraAngleFunctionB * targetAngle + RobotMap.cameraAngleFunctionC;
+		return targetDistance;
+    }
+    
+    public void activateLightRing() {
+    	ringLight.setDirection(Relay.Direction.kReverse);
+    	ringLight.set(Relay.Value.kOn);
+    }
+    
+    public void DeactivateLightRing() {
+    	ringLight.set(Relay.Value.kOff);
+    }
+    
+    public void setCameraServo(double pos) {
+    	tiltServo.set(pos);
+    }
+    
+    public double getCameraServo() {
+    	return tiltServo.get();
     }
 }
 
