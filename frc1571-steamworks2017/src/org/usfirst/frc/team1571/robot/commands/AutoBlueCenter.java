689b819205932d4a40b86fa36e922c8a04f481a5
SamuelFrazee <samuelfrazee@hotmail.com>
2017-02-06 18:02:40 -0800

    Added test commands to auto command group

--- frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/AutoBlueCenter.java
+++ frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/AutoBlueCenter.java
@@ -8,6 +8,19 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class AutoBlueCenter extends CommandGroup {
 
     public AutoBlueCenter() {
+        addParallel(new LEDTheaterChase(255,255,0,20));
         addSequential(new DriveDistance(2.5,1,0));
+        
+        addParallel(new LEDRainbowCycle(5));
+        addSequential(new WaitForGearRemoval());
+        
+        addParallel(new LEDTheaterChase(255,255,0,20));
+        addSequential(new DriveDistance(-2.5,-1,0));
+        addSequential(new TurnAngle(1,-90));
+        addSequential(new DriveDistance(2.5,1,0));
+        
+        addSequential(new Aimbot());
+        
+        
     }
 }
