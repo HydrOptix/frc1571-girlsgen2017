2a09aeb2641c1630048e93e01788300cb1f39617
SamuelFrazee <samuelfrazee@hotmail.com>
2017-02-06 18:08:54 -0800

    Added code for DriveSpeed command
    DriveSpeed will now use DriveSystem to set the speed based on command
    input

--- frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/DriveSpeed.java
+++ frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/DriveSpeed.java
@@ -1,36 +1,25 @@
 package org.usfirst.frc.team1571.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import org.usfirst.frc.team1571.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.InstantCommand;
 
 /**
  *
  */
-public class DriveSpeed extends Command {
+public class DriveSpeed extends InstantCommand {
+	
+	double speed, steering;
 
     public DriveSpeed(double speed, double steering) {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+    	requires(Robot.driveSystem);
+        this.speed = speed;
+        this.steering = steering;
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
+    	System.out.println("Driving Speed");
+    	Robot.driveSystem.tankDrive(speed, steering);
     }
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
 }
