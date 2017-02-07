70a22ca97548564a272a6dfb27c5d7854c1e5951
SamuelFrazee <samuelfrazee@hotmail.com>
2017-02-06 18:15:52 -0800

    Added required subsystems and direct subsystem control
    The JoystickManager command no longer relies on fixed commands and now
    addressed the required subsystems directly. It also reports all of its
    used variables to the dashboard.

--- frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/JoystickManager.java
+++ frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/JoystickManager.java
@@ -4,18 +4,23 @@ import org.usfirst.frc.team1571.robot.Robot;
 import org.usfirst.frc.team1571.robot.RobotMap;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 
 
 public class JoystickManager extends Command {
 
     public JoystickManager() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
+    	requires(Robot.agitator);
+    	requires(Robot.climber);
+    	requires(Robot.driveSystem);
+    	requires(Robot.feeder);
+    	requires(Robot.shooter);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+    	System.out.println("Starting Joystick Manager");
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -27,6 +32,15 @@ public class JoystickManager extends Command {
     	boolean driverButtonX = Robot.oi.driverButtonX.get();
     	boolean auxButtonTrigger = Robot.oi.auxButtonTrigger.get();
     	boolean auxButtonSecondary = Robot.oi.auxButtonSecondary.get();
+    	SmartDashboard.putNumber("Left Joystick X", driverXLeft);
+    	SmartDashboard.putNumber("Left Trigger", driverTriggerLeft);
+    	SmartDashboard.putNumber("Right Trigger", driverTriggerRight);
+    	SmartDashboard.putNumber("Joystick Twist Axis", auxTwistAxis);
+    	SmartDashboard.putBoolean("X Button", driverButtonX);
+    	SmartDashboard.putBoolean("Joystick Trigger", auxButtonTrigger);
+    	SmartDashboard.putBoolean("Joystick Secondary Button", auxButtonSecondary);
+    	SmartDashboard.putBoolean("Driving", RobotMap.driving);
+
     	
     	if(Math.abs(driverXLeft) < Robot.oi.driverController_deadzoneRadiusLStick) {
     		driverXLeft = 0;
@@ -40,41 +54,44 @@ public class JoystickManager extends Command {
     	if(Math.abs(auxTwistAxis) < Robot.oi.auxJoystick_deadzoneRadiusTwist) {
     		auxTwistAxis = 0;
     	}
-    	
+    	    	    	
     	if(driverXLeft == 0 && driverTriggerLeft == 0 && driverTriggerRight == 0 && !driverButtonX) {	//If the the driver is doing anything to drive the robot,
     		RobotMap.driving = false;																	//set the driving variable to true. Otherwise, set the driving variable to false.
     	} else {
     		RobotMap.driving = true;
     	}
-    	
+    	    	
     	if(!driverButtonX) {
     	
 	    	if(RobotMap.driving) {	//If the driver is trying to drive the robot, run the commands needed 
 	    		double driveThrottle = driverTriggerRight - driverTriggerLeft; //Find the driving speed by combining the two gamepad trigger inputs
+	    		SmartDashboard.putNumber("Virtual Throttle", driveThrottle);
 	    		
 	    		if(driveThrottle == 0) { //If the robot isn't supposed to be moving forward (throttle is 0) then run the stationary turning command
 	    			if(driverXLeft != 0) {
-	    				new TurnSpeed(driveThrottle);
+	    				System.out.println("Turning");
+	    				Robot.driveSystem.stationaryTurn(driveThrottle);
 	    			}
 	    		} else {				//If the robot is supposed to be moving forward (throttle is not 0) then run the driving/reduced turning code
-	    			new DriveSpeed(driveThrottle, driverXLeft);
+	    			System.out.println("Driving");
+	    			Robot.driveSystem.tankDrive(driveThrottle, driverXLeft);
 	    		}
 	    	} else {				//If the driver is not interacting with the robot, run the secondary joystick shooting code
+	    		double targetSpeed;
 	    		if(auxButtonSecondary) { //If the secondary button is pressed, auto-detect the speed the flywheels need to be running
-	    			double distance = Robot.cameraSystem.getVisionDistance();
-	    			double targetSpeed = distance; //TODO - distance equation
-	    			new SetShootSpeed(targetSpeed);
+	    			double distance = Robot.cameraSystem.getTargetDistance();
+	    			targetSpeed = distance; //TODO - distance equation
 	    		} else { //If the secondary button is not pressed, run the flywheels at the speed specified by the throttle
-	    			double targetSpeed = (Robot.oi.auxJoystick.getRawAxis(3)-1)/-2;
-	    			new SetShootSpeed(targetSpeed);
+	    			targetSpeed = (Robot.oi.auxJoystick.getRawAxis(3)-1)/-2;
 	    		}
+    			Robot.shooter.setSpeed(targetSpeed);
 	    		
 	    		if(auxButtonTrigger) {
-	    			new StartFeeder();
-	    			new StartAgitator();
+	    			Robot.feeder.startFeeder();
+	    			Robot.agitator.startAgitator();
 	    		} else {
-	    			new StopFeeder();
-	    			new StopAgitator();
+	    			Robot.feeder.stopFeeder();
+	    			Robot.agitator.stopAgitator();
 	    		}
 	    		
 	    	}
