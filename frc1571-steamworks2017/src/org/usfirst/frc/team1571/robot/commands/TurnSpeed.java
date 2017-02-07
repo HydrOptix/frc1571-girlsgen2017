1616ca53ed5b70c821e2f715d23122e153931c91
SamuelFrazee <samuelfrazee@hotmail.com>
2017-02-06 18:23:47 -0800

    Changed command to InstantCommand and added code

--- frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/TurnSpeed.java
+++ frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/TurnSpeed.java
@@ -2,37 +2,22 @@ package org.usfirst.frc.team1571.robot.commands;
 
 import org.usfirst.frc.team1571.robot.Robot;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 
 /**
  *
  */
-public class TurnSpeed extends Command {
+public class TurnSpeed extends InstantCommand {
+	
+	double speed;
 
     public TurnSpeed(double speed) {
-        // Use requires() here to declare subsystem dependencies
         requires(Robot.driveSystem);
+        
+        this.speed = speed;
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
-    }
-
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
-
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
-
-    // Called once after isFinished returns true
-    protected void end() {
-    }
-
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
+    	Robot.driveSystem.stationaryTurn(speed);
     }
 }
