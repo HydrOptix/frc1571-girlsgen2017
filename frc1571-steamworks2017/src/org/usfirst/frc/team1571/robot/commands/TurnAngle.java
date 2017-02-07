674ebe6d9e8015d9b81a0dc8331b91fa6bde1ea9
SamuelFrazee <samuelfrazee@hotmail.com>
2017-02-06 18:23:11 -0800

    Added code to turn to a certain angle

--- frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/TurnAngle.java
+++ frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/TurnAngle.java
@@ -1,38 +1,50 @@
 package org.usfirst.frc.team1571.robot.commands;
 
 import org.usfirst.frc.team1571.robot.Robot;
-
 import edu.wpi.first.wpilibj.command.Command;
 
-/**
- *
- */
 public class TurnAngle extends Command {
+	
+	double speed, angle, lastAngle;
+	boolean isFinished = false;
 
-    public TurnAngle() {
-        // Use requires() here to declare subsystem dependencies
-        requires(Robot.driveSystem);
+    public TurnAngle(double speed, double angle) {
+    	this.speed = speed;
+    	this.angle = angle;
     }
 
-    // Called just before this Command runs the first time
     protected void initialize() {
+    	isFinished = true;
+    	lastAngle = Robot.driveSystem.getGyroAngle();
+    	new TurnSpeed(speed);
     }
 
-    // Called repeatedly when this Command is scheduled to run
     protected void execute() {
+    	double currentAngle = Robot.driveSystem.getGyroAngle();
+    	if(speed > 0) {
+    		if(lastAngle < angle && currentAngle > angle) {
+    			isFinished = true;
+    		}
+    	} else if(speed < 0) {
+    		if(lastAngle > angle && currentAngle < angle) {
+    			isFinished = true;
+    		}
+    	} else {
+    		isFinished = true;
+    		System.out.print("Warning - Tried to turn to an angle with no speed. Why are you doing this?");
+    	}
+    	lastAngle = currentAngle;
     }
 
-    // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+        return isFinished;
     }
 
-    // Called once after isFinished returns true
     protected void end() {
+    	new TurnSpeed(0);
     }
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
     protected void interrupted() {
+    	System.out.println("Warning - Turn to angle interrupted, robot may be misaligned.");
     }
 }
