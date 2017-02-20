1bfbb5b5050b894b61c4899a5413c8bb15510ef6
SamuelFrazee <samuelfrazee@hotmail.com>
2017-02-19 20:23:05 -0800

    Added a feeder toggling function

--- frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/subsystems/Feeder.java
+++ frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/subsystems/Feeder.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team1571.robot.subsystems;
 
 import org.usfirst.frc.team1571.robot.RobotMap;
+import org.usfirst.frc.team1571.robot.commands.StopFeeder;
 
 import com.ctre.CANTalon;
 
@@ -12,16 +13,28 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Feeder extends Subsystem {
 	
 	private final CANTalon feederTalon = RobotMap.feederTalon;
+	private boolean feederRunning;
 
     public void initDefaultCommand() {
+    	setDefaultCommand(new StopFeeder());
     }
     
     public void startFeeder() {
     	feederTalon.set(RobotMap.feederSpeed);
+    	feederRunning = true;
     }
     
     public void stopFeeder() {
     	feederTalon.set(0);
+    	feederRunning = false;
+    }
+    
+    public void toggleFeeder() {
+    	if(feederRunning) {
+    		stopFeeder();
+    	} else {
+    		startFeeder();
+    	}
     }
 }
 
