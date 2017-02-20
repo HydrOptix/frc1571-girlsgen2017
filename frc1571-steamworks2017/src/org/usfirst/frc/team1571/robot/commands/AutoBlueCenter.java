f2a861ca61e41d493766c75b9a5d6d4fe9386f16
SamuelFrazee <samuelfrazee@hotmail.com>
2017-02-19 20:17:32 -0800

    Re-added blue center autonomous

--- frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/AutoBlueCenter.java
+++ frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/commands/AutoBlueCenter.java
@@ -2,25 +2,21 @@ package org.usfirst.frc.team1571.robot.commands;
 
 import edu.wpi.first.wpilibj.command.CommandGroup;
 
-/**
- *
- */
 public class AutoBlueCenter extends CommandGroup {
 
     public AutoBlueCenter() {
-        addParallel(new LEDTheaterChase(255,255,0,20));
+        addParallel(new LEDTheaterChase(0,255,0,10));
         addSequential(new DriveDistance(2.5,1,0));
         
         addParallel(new LEDRainbowCycle(5));
         addSequential(new WaitForGearRemoval());
         
-        addParallel(new LEDTheaterChase(255,255,0,20));
+        addParallel(new LEDTheaterChase(255,0,0,10));
         addSequential(new DriveDistance(-2.5,-1,0));
         addSequential(new TurnAngle(1,-90));
         addSequential(new DriveDistance(2.5,1,0));
         
+        addParallel(new LEDRainbowCycle(5));
         addSequential(new Aimbot());
-        
-        
     }
 }
