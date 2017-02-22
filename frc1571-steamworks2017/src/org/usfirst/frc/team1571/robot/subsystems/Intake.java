a091dd3adc17ef153c251333fed23c14aff396df
SamuelFrazee <samuelfrazee@hotmail.com>
2017-02-22 09:17:18 -0800

    Added toggle intake toggling function

--- frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/subsystems/Intake.java
+++ frc1571-steamworks2017/src/org/usfirst/frc/team1571/robot/subsystems/Intake.java
@@ -9,15 +9,26 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 public class Intake extends Subsystem {
 	
 	private final CANTalon intakeTalon = RobotMap.intakeTalon;
+	private boolean intakeRunning;
 
 	public void initDefaultCommand() {
 	}
 	
 	public void startIntake() {
 		intakeTalon.set(RobotMap.intakeSpeed);
+		intakeRunning = true;
 	}
 	
 	public void stopIntake() {
 		intakeTalon.set(0);
+		intakeRunning = false;
 	}
+	
+	public void toggleIntake() {
+    	if(intakeRunning) {
+    		stopIntake();
+    	} else {
+    		startIntake();
+    	}
+    }
 }
